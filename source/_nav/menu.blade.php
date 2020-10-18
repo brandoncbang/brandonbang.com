ee3ecb8f960040351cb4292fb980e473e597db89
thermalsquid <36747510+thermalsquid@users.noreply.github.com>
2020-10-18 19:01:09 -0400

    Add responsive nav menu

--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -1,16 +1,32 @@
-<nav class="hidden lg:flex items-center justify-end text-lg">
-    <a title="{{ $page->siteName }} Blog" href="/blog"
-        class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/blog') ? 'active text-blue-600' : '' }}">
-        Blog
-    </a>
-
-    <a title="{{ $page->siteName }} About" href="/about"
-        class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/about') ? 'active text-blue-600' : '' }}">
-        About
-    </a>
-
-    <a title="{{ $page->siteName }} Contact" href="/contact"
-        class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/contact') ? 'active text-blue-600' : '' }}">
-        Contact
-    </a>
+<nav id="js-nav-menu" class="hidden absolute md:block md:relative z-10 top-0 bottom-0 left-0 right-0 bg-green-700 md:bg-transparent">
+    <ul class="flex flex-col md:flex-row items-center md:justify-end h-full m-0 list-none text-2xl">
+        <li class="mt-24 md:mt-0 mb-8 md:mb-0 md:mr-8">
+            <a href="/services/" class="text-white md:text-green-700 hover:text-green-500">Services</a>
+        </li>
+        <li class="mb-8 md:mb-0 md:mr-8">
+            <a href="/about/" class="text-white md:text-green-700 hover:text-green-500">About Me</a>
+        </li>
+        <li class="">
+            <a href="/contact/" class="text-white md:text-green-700 hover:text-green-500">Contact</a>
+        </li>
+    </ul>
 </nav>
+
+<button class="md:hidden flex z-20 hamburger hamburger--spin" type="button">
+    <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+    </span>
+</button>
+
+@push('scripts')
+<script>
+    (() => {
+        let navMenu = document.querySelector('#js-nav-menu');
+        let hamburger = document.querySelector('.hamburger');
+        hamburger.addEventListener('click', (event) => {
+            hamburger.classList.toggle('is-active');
+            navMenu.classList.toggle('hidden');
+        });
+    })();
+</script>
+@endpush
