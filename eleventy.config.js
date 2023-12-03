38bbb35d7798d4c1f51b781c2deaff157a2ec224
Brandon Bang <brandoncharlesbang@gmail.com>
2023-12-03 13:01:15 -0500

    Add portrait photo

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,21 +1,22 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget('./_site/css/')
-  eleventyConfig.addPassthroughCopy('./src/js/')
+  eleventyConfig.addWatchTarget("./_site/css/");
+  eleventyConfig.addPassthroughCopy("./src/img/");
+  eleventyConfig.addPassthroughCopy("./src/js/");
 
-  eleventyConfig.addFilter('head', (array, n = 1) => {
+  eleventyConfig.addFilter("head", (array, n = 1) => {
     if (!Array.isArray(array) || array.length === 0) {
-      return []
+      return [];
     }
     if (n < 0) {
-      return array.slice(n)
+      return array.slice(n);
     }
 
-    return array.slice(0, n)
-  })
+    return array.slice(0, n);
+  });
 
   return {
     dir: {
-      input: 'src',
+      input: "src",
     },
-  }
-}
+  };
+};
--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  semi: false,
-  singleQuote: true,
-  plugins: ['@shopify/prettier-plugin-liquid', 'prettier-plugin-tailwindcss'],
-}
+  plugins: ["@shopify/prettier-plugin-liquid", "prettier-plugin-tailwindcss"],
+};
