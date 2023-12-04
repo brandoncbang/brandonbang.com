7a8b2e2aa1e0eff7e969f399d75cbb5d81737751
Brandon Bang <brandoncharlesbang@gmail.com>
2023-12-03 19:32:39 -0500

    Customize fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,liquid,js}'],
+  content: ["./src/**/*.{html,liquid,js}"],
   theme: {
     extend: {
-      colors: {
-        lime: {
-          25: '#fbfef3',
-        },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        display: ["Lexend", "sans-serif"],
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
