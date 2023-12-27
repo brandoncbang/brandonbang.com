c2268bee7dcf2dab081419b214da683acfcc6aec
Brandon Bang <brandoncharlesbang@gmail.com>
2023-12-27 10:49:21 -0500

    Add syntax highlighting

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,9 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.addWatchTarget("./_site/css/");
   eleventyConfig.addPassthroughCopy("./src/favicons/");
